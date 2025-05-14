cf494cf9f5ca8fa533902e1acf4cab26fcd6be2c
lakshyachaudhry <lakshchaudhry@gmail.com>
2025-05-13 23:43:40 -0700

    Rotating 3D text with a custom font in React Native

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-
 // This is a simple React component that displays a greeting message.
 // It uses Tailwind CSS for styling and is set up to be used with Vite.
 // This is also my first react component!
+
+import Hero3D from "./components/Hero3D";
+import "./App.css";
+
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-5xl text-white font-bold">Hello, Sachin!</h1>
+    <div className="my-background">
+      { <Hero3D /> }
     </div>
   );
+
 }
