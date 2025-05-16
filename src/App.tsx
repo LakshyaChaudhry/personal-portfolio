ca4edbc80381a9d930151231e86af4daf1c8e0be
lakshyachaudhry <lakshchaudhry@gmail.com>
2025-05-16 06:54:59 -0700

    Adding page sections V1

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,35 @@
 // This is also my first react component!
 
 import Hero3D from "./components/Hero3D";
+import React from "react";
 import "./App.css";
+
+
 
 export default function App() {
   return (
+    <>
     <div className="my-background">
       { <Hero3D /> }
     </div>
+
+    <div className="horizontal-scroll-container">
+      <div className="page-section">
+        <h1> Welcome Page </h1>
+        <p>This is the first section of my portfolio</p>
+      </div>
+
+      <div className="page-section">
+        <h1> Coding Projects </h1>
+        <p>This is a showcase of my coding work</p>
+      </div>
+
+      <div className="page-section">
+        <h1> Blog </h1>
+        <p>This is a collection of my blog posts</p>
+      </div>
+    </div>
+    </>
   );
 
 }
