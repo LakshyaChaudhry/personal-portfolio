2d20e6a96a0aa734d5b9439cb5561a42d5754778
lakshyachaudhry <lakshchaudhry@gmail.com>
2025-05-27 21:17:08 -0700

    added tailwindcss & postcss dependency and updated PostCSS configuration to match with new versions

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../Experience.css';
+import Component from './timeline.tsx';
 
 function Experience() {
     return (
         <div className="page-section">
+            <Component />
             <h1> Experience </h1>
             <p>This is a showcase of my work experience</p>
         </div>
